ac2e616ecffe1d31af616a936ba0094cdb1e1881
Daria <kd7614@yandex.ru>
2021-03-15 00:20:04 +0300

    dialog design

--- app/src/main/java/com/example/weatherapp/ui/dialogs/MapDialog.java
+++ app/src/main/java/com/example/weatherapp/ui/dialogs/MapDialog.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -49,42 +50,78 @@ public class MapDialog extends AppCompatDialogFragment {
 //        intent.putExtra("lon", lon);
 //        intent.putExtra("cityName", cityName);
 
-//        LayoutInflater inflater = getActivity().getLayoutInflater();
-//        View view = inflater.inflate(R.layout.layout_dialog, null);
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_dialog, null);
+        Button nextBtn = view.findViewById(R.id.nextBtn);
+        Button cancelBtn = view.findViewById(R.id.cancelBtn);
+        RadioGroup radioGroup = view.findViewById(R.id.radioGroup);
+
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selecredId = radioGroup.getCheckedRadioButtonId();
+                switch (selecredId) {
+                    case (R.id.radioBtnYandex):
+                        intent = new Intent(getContext(), YandexMaps.class);
+                        intent.putExtra("lat", lat);
+                        intent.putExtra("lon", lon);
+                        intent.putExtra("cityName", cityName);
+                        break;
+                    case (R.id.radioBtnGoogle):
+                        intent = new Intent(getContext(), GoogleMaps.class);
+                        intent.putExtra("lat", lat);
+                        intent.putExtra("lon", lon);
+                        intent.putExtra("cityName", cityName);
+                        break;
+                }
+                if (intent != null) {
+                    startActivity(intent);
+                    dismiss();
+                }
+            }
+        });
 
         CharSequence[] values = {"Google Map", "Yandex Map"};
-        builder.setTitle("Choose maps")
-                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                })
-                .setPositiveButton("next", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        if (intent!=null) startActivity(intent);
-                        else Toast.makeText(getContext(), "Intent is null", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .setSingleChoiceItems(values, 1, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Log.d("Dialog", String.valueOf(which));
-                        if (which==0) {
-                            intent = new Intent(getContext(), GoogleMaps.class);
-                            intent.putExtra("lat", lat);
-                            intent.putExtra("lon", lon);
-                            intent.putExtra("cityName", cityName);
-                        } else {
-                            intent = new Intent(getContext(), YandexMaps.class);
-                            intent.putExtra("lat", lat);
-                            intent.putExtra("lon", lon);
-                            intent.putExtra("cityName", cityName);
-                        }
-
-                    }
-                });
+        builder.setView(view)
+                .setTitle("Выберите карту");
+//                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//
+//                    }
+//                })
+//                .setPositiveButton("next", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        if (intent!=null) startActivity(intent);
+//                        else Toast.makeText(getContext(), "Intent is null", Toast.LENGTH_SHORT).show();
+//                    }
+//                })
+//                .setSingleChoiceItems(values, 1, new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        Log.d("Dialog", String.valueOf(which));
+//                        if (which==0) {
+//                            intent = new Intent(getContext(), GoogleMaps.class);
+//                            intent.putExtra("lat", lat);
+//                            intent.putExtra("lon", lon);
+//                            intent.putExtra("cityName", cityName);
+//                        } else {
+//                            intent = new Intent(getContext(), YandexMaps.class);
+//                            intent.putExtra("lat", lat);
+//                            intent.putExtra("lon", lon);
+//                            intent.putExtra("cityName", cityName);
+//                        }
+//
+//                    }
+//                });
         alertDialog = builder.create();
         alertDialog.show();
         return alertDialog;
